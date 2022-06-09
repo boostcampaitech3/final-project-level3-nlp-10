c06775a687164bc9555ac75b2e0b0887a65a008a
Jeongho Ju <jujeongho@ajou.ac.kr>
2022-06-09 15:19:05 +0900

    Update app.py

--- code/app/app.py
+++ code/app/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+import os
 import streamlit as st
 from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast, TextClassificationPipeline, BertForSequenceClassification, AutoTokenizer
 import torch
